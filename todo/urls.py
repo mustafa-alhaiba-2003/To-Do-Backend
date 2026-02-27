5b57eae56eb2a2b4ced7a0ff747477386bf5df11
mustafa-alhaiba-2003 <mustafaalhaiba117@gmail.com>
2026-02-27 16:48:27 +0300

    add static urls for media files

--- todo/urls.py
+++ todo/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("apps.user.urls")),
     path("api/tasks/", include("apps.tasks.urls")),
     path("api/admin/", include("apps.dashboard.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
